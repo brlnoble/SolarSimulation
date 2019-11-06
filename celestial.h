ca2d3a07eb07ad1f8da4417da89792bec6f8ca38
brlnoble <51976754+brlnoble@users.noreply.github.com>
2019-11-06 12:16:40 -0500

    Update celestial.h

--- celestial.h
+++ celestial.h
@@ -1,22 +1,8 @@
-#include<cmath>
 class celestial {
 public:
-	float mass; //mass in solar units, our sun will be 1.0
-	float x,y,vX,vY; //position in velocity according to axis
-	float T,R,w; //period,radius from center of mass,angular velocity
-	float vT; //tangential velocity
-	float radStep; //radial step between samples
-	float thetaC; //theta old and theta current
-	float dt;
-	
-	float a,b,e; //elliptical properties of orbit
-	
-	void updateV() {
-		vX=-vT*sin(thetaC);
-		vY=vT*cos(thetaC);
-	}
-	void updatePos() {
-		x+=vX*dt;
-		y+=vY*dt;
-	}
+	float m; //mass in solar units, our sun will be 1.0
+	float X,Y,Vx,Vy; //position in velocity according to axis
+	float R; //radius from center of mass
+	float Vt; //tangential velocity
+	float aX, aY;
 };
