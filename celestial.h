cdb607deb81c154cc843ab42e0cd5fd28ebed083
brlnoble <51976754+brlnoble@users.noreply.github.com>
2019-11-13 10:02:39 -0500

    Update celestial.h
    Added a function to approximate colour from mass. This is inaccurate as colour is dependent mostly on temperature, however for the purpose of this simple model it will suffice. Function will be used later on for a full galaxy simulation.

--- celestial.h
+++ celestial.h
@@ -1,8 +1,17 @@
 class celestial {
 public:
-	float m; //mass in solar units, our sun will be 1.0
-	float X,Y,Vx,Vy; //position in velocity according to axis
-	float R; //radius from center of mass
-	float Vt; //tangential velocity
-	float aX, aY;
+
+	float m; //mass in solar units, the Sun will be 1.0
+	float X,Y,Vx,Vy; //position in velocity according to axis in units of AU/year
+	float R; //radius from center of mass, intermediate variable for calculations
+	float aX, aY; //X and Y acceleration components
+	
+	int colour; //colour used to plot the object, dependent on mass to replicate
+	
+	void updateCol() { //change colour depending on mass, attempting to replicate real star colours
+		if(m<1.0) colour = 2; //red
+		else if(m>=1.0 && m<5.0) colour = 7; //yellow
+		else if(m>=5.0 && m<10.0) colour = 5; //blue
+		else colour = 1; //white
+	};
 };
