b968b5b46f0739701e560a2b23f7b104f690fbe3
brlnoble <51976754+brlnoble@users.noreply.github.com>
2019-11-20 13:28:29 -0500

    Update celestial.h
    Added basic collision handling.

--- celestial.h
+++ celestial.h
@@ -5,6 +5,7 @@ public:
 	float X,Y,Vx,Vy; //position in velocity according to axis in units of AU/year
 	float R; //radius from center of mass, intermediate variable for calculations
 	float aX, aY; //X and Y acceleration components
+	bool state; //used to determine if the current body is alive or not, used for collisions
 	
 	int colour; //colour used to plot the object, dependent on mass to replicate
 	
@@ -13,5 +14,22 @@ public:
 		else if(m>=1.0 && m<5.0) colour = 7; //yellow
 		else if(m>=5.0 && m<10.0) colour = 5; //blue
 		else colour = 1; //white
+	};
+	void collision(celestial bod) { //kill one body, moving its mass to the other one
+		if(bod.m == 1000.0) state = false;
+		else if(m >= bod.m) {
+			bod.state = false;
+			m += bod.m;
+			updateCol();
+			Vx += bod.Vx;
+			Vy += bod.Vy;
+		}
+		else {
+			state = false;
+			bod.m += m;
+			bod.updateCol();
+			bod.Vx -= Vx;
+			bod.Vy -= Vy;
+		}
 	};
 };
