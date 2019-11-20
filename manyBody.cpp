8f759e42e01de5edbc64cf9ba456996eedde2a47
brlnoble <51976754+brlnoble@users.noreply.github.com>
2019-11-20 13:06:25 -0500

    Update manyBody.cpp
    Better stability for multiple objects. Added in collisions with decent results. Still need work for greater range of initial positions and better generated initial conditions. Plotting to be moved over to matplotlib in Python soon. See image result dated November 20th for current progress.

--- manyBody.cpp
+++ manyBody.cpp
@@ -19,7 +19,7 @@ int main() {
 	float dt = 0.01; //time step
 	float dx,dy,a;
 	float e = 0.01; //offset to try and avoid slingshotting
-	float scale = 60.0; //scale of graph
+	float scale = 15.0; //scale of graph
 
 	//##### Set up plot window #####
 	if(!cpgopen("/XWINDOW")) return 1; //open window
@@ -32,11 +32,13 @@ int main() {
 	
 	celestial obj[N]; //create array of bodies using self made Celestial class
 	
-	obj[0].m = 100.0; //supermassive blackhole at centre of galaxy, do not move it
+	obj[0].m = 1000.0; //supermassive blackhole at centre of galaxy, do not move it
 	obj[0].X = 0.0;
 	obj[0].Y = 0.0;
+	obj[0].state = true;
 	
 	for(i=1; i<N; i++) {
+		obj[i].state = true;
 		obj[i].m = Random(0.5, 3.0);
 		obj[i].updateCol();
 		obj[i].X = Random(-8.0, 8.0);
@@ -45,17 +47,19 @@ int main() {
 		//obj[i].Vy = Random(-4.0, -1.0);
 		dx = obj[i].X-obj[0].X; //first body
 		dy = obj[i].Y-obj[0].Y;
-		obj[i].Vy = 0.5*sqrt(G*obj[0].m/sqrt(dx*dx+e*e))*dx/sqrt(dx*dx+e*e);
-		obj[i].Vx = -0.5*sqrt(G*obj[0].m/sqrt(dy*dy+e*e))*dy/sqrt(dy*dy+e*e);
-		cout<<"Body "<<i<<"\n\tm: "<<obj[i].m<<"\n\tX,Y: "<<obj[i].X<<","<<obj[i].Y<<"\n\tVx,Vy: "<<obj[i].Vx<<","<<obj[i].Vy<<"\n";
+		obj[i].Vy = 0.25*sqrt(G*obj[0].m/sqrt(dx*dx+e*e))*dx/sqrt(dx*dx+e*e);
+		obj[i].Vx = -0.25*sqrt(G*obj[0].m/sqrt(dy*dy+e*e))*dy/sqrt(dy*dy+e*e);
+		cout<<"Body "<<i<<"\n\tm: "<<obj[i].m<<"\n\tX,Y: "<<obj[i].X<<","<<obj[i].Y<<"\n\tVx,Vy: "<<obj[i].Vx<<","<<obj[i].Vy<<"\n\tState: "<<obj[i].state<<"\n";
 		cpgpt(1,&obj[i].X,&obj[i].Y,2);
 	}
 	
 	//##### Loop calculations ######
 	for(i=0; i<10000000; i++) { //using leapfrog method to calculate orbit
 		for(j=1;j<N;j++) { //pick body affected
+		if(obj[j].state == false) continue;
 			for(k=0;k<N;k++) { //pick body acting on selected one
 				if(j==k) continue; //skip itself
+				if(obj[k].state == false) continue;
 				//Leapfrog with position first
 				
 				obj[j].X += (dt/2)*obj[j].Vx;
@@ -66,6 +70,20 @@ int main() {
 				
 				
 				obj[j].R = sqrt( e*e + dx*dx + dy*dy ); //add e^e to prevent R from being zero, prevents slingshotting
+				if(k == 0) {
+					if(obj[j].R <= 0.15) {
+						obj[j].collision(obj[k]);
+						cout<<"Collision between "<<j<<" and "<<k<<"\n";
+						continue;
+					}
+					else {
+						if(obj[j].R <= 0.05) {
+							obj[j].collision(obj[k]);
+							cout<<"Collision between "<<j<<" and "<<k<<"\n";
+							continue;
+						}
+					}
+				}
 				
 				a = -G*(obj[k].m/obj[j].m)/(obj[j].R*obj[j].R);
 				
@@ -78,11 +96,13 @@ int main() {
 				obj[j].X += (dt/2)*obj[j].Vx;
 				obj[j].Y += (dt/2)*obj[j].Vy;
 			} //END CALCULATION GROUP
-		//cpgeras();
-		cpgsci(obj[j].colour); //follows colour of the given body
-		cpgpt(1,&obj[j].X,&obj[j].Y,1); //draw the current position as a point
+			//cpgeras();
+			if(obj[j].state == true) {
+				cpgsci(obj[j].colour); //follows colour of the given body
+				cpgpt(1,&obj[j].X,&obj[j].Y,1); //draw the current position as a point
+			}
 		} //END BODY CHOOSING LOOP
-		usleep(500);
+		usleep(50000);
 	} //END MAIN LOOP
 }
 
