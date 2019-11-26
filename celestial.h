093a4c8ab9d991d5f23cb35bd8cc8684116de018
brlnoble <51976754+brlnoble@users.noreply.github.com>
2019-11-26 18:19:40 -0500

    Update celestial.h

--- celestial.h
+++ celestial.h
@@ -2,9 +2,9 @@ class celestial {
 public:
 
 	float m; //mass in solar units, the Sun will be 1.0
-	float X,Y,Vx,Vy; //position in velocity according to axis in units of AU/year
+	float X,Y,Z,Vx,Vy,Vz; //position in velocity according to axis in units of AU/year
 	float R; //radius from center of mass, intermediate variable for calculations
-	float aX, aY; //X and Y acceleration components
+	float aX, aY, aZ; //X and Y acceleration components
 	bool state; //used to determine if the current body is alive or not, used for collisions
 	
 	int colour; //colour used to plot the object, dependent on mass to replicate
@@ -21,15 +21,15 @@ public:
 			bod.state = false;
 			m += bod.m;
 			updateCol();
-			Vx += bod.Vx;
-			Vy += bod.Vy;
+			//Vx -= bod.Vx*(X/R); not being used, to be fixed later
+			//Vy -= bod.Vy*(Y/R);
 		}
 		else {
 			state = false;
 			bod.m += m;
 			bod.updateCol();
-			bod.Vx -= Vx;
-			bod.Vy -= Vy;
+			//bod.Vx -= Vx*(X/R); not being used, to be fixed later
+			//bod.Vy -= Vy*(Y/R);
 		}
 	};
 };
